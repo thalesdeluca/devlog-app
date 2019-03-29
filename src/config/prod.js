2e36b9949db794591e10d48a54156c07d791d989
Thales de Luca <delucathales@gmail.com>
2019-03-29 20:48:21 -0300

    Added start script

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,3 +1,8 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI
+  mongoURI: process.env.MONGO_URI,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  tokenSecret: process.env.TOKEN_SECRET 
 }
